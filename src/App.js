b80ba2ffa2ad4bb5ecedbb81ddf929f121b9bef4
alewaya <a.nicastro@wayassauto.com>
2025-07-19 17:53:05 +0200

    Complete the tutorial. Now the game allows players to jump to previous moves, and the current player is determined based on the move number.

--- src/App.js
+++ src/App.js
@@ -9,9 +9,7 @@ function Square({value, onSquareClick}) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+function Board({xIsNext, squares, onPlay}) {
 
   let status;
   const winner = calculateWinner(squares);
@@ -26,8 +24,7 @@ export default function Board() {
     const nextSquares = squares.slice();
     if (xIsNext) {nextSquares[i] = 'X';}
     else {nextSquares[i] = 'O';}
-    setXIsNext(!xIsNext);
-    setSquares(nextSquares);
+    onPlay(nextSquares);
   };
 
   return (
@@ -49,6 +46,53 @@ export default function Board() {
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
       </div>
     </>
+  );
+}
+
+
+export default function Game(){
+
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  function jumpTo(move){
+    setCurrentMove(move);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>
+          {description}
+        </button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
   );
 }
 
